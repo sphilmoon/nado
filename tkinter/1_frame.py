f40ccebd184e1d582956518acd9e5c79c44045c9
sphilmoon <sphilmoon@gmail.com>
2021-01-07 18:45:35 +0900

    created GUI repository with a frame script.

---
+++ tkinter/1_frame.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+# opening the GUI window
+root = Tk()
+
+root.title("Nado GUI")
+root.geometry("640x480")
+#root.geometry("640x480+300+100") # w x h + x + y coordinants. 
+root.resizable(False, False) # x, y resizable is disabled. 
+
+# keeping the GUI window open. 
+root.mainloop()
